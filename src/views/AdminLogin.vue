19b2b285ccdbcaa5c1c9932ee29a124e3668a7d7
smich254 <smich254@gmail.com>
2023-08-30 05:29:30 -0500

    Primer commit

---
+++ src/views/AdminLogin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Login Admin</h1>
+    <!-- Aquí irá tu formulario de inicio de sesión de administrador -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminLogin",
+  data() {
+    return {
+      // tus datos aquí
+    };
+  },
+  methods: {
+    // tus métodos aquí
+  },
+};
+</script>
+
+<style scoped>
+/* tus estilos aquí */
+</style>
